1d0ecd633000adcf4000e48d23fcb19362d482f6
Kamalakanta Dalai <105095542+Kamalakanta-Dalai@users.noreply.github.com>
2022-12-29 12:25:54 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,79 @@
+// const name = prompt("Hey..What's Your Name?");
+
+// alert(`Hello ${name}, Welcome to KK Drum Kit`);
+//For detecting click event
+for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+    var buttonInNerHTML = this.innerHTML;
+    makeSound(buttonInNerHTML);
+    buttonAnimation(buttonInNerHTML);
+  });
+}
+
+//FOR DETECTING KEYBOARD PRESS
+
+// document.addEventListener("keypress", myFunction);
+
+// function myFunction() {
+//   alert("i'm clicked");
+// }
+// document.addEventListener("keypress", function () {
+//   alert("i'm clicked");
+// });
+document.addEventListener("keypress", function (event) {
+  // instead of (event) u can also write (e)
+  // console.log(event.key); //here also instead of .key u can use ["key"]
+  makeSound(event.key);
+  buttonAnimation(event.key);
+});
+
+function makeSound(key) {
+  switch (key) {
+    case "w":
+      var audio = new Audio("sounds/tom-1.mp3");
+      audio.play();
+
+      break;
+    case "a":
+      var audio = new Audio("sounds/tom-2.mp3");
+      audio.play();
+
+      break;
+    case "s":
+      var audio = new Audio("sounds/tom-3.mp3");
+      audio.play();
+
+      break;
+    case "d":
+      var audio = new Audio("sounds/tom-4.mp3");
+      audio.play();
+
+      break;
+    case "j":
+      var audio = new Audio("sounds/snare.mp3");
+      audio.play();
+
+      break;
+    case "k":
+      var audio = new Audio("sounds/crash.mp3");
+      audio.play();
+
+      break;
+    case "l":
+      var audio = new Audio("sounds/kick-bass.mp3");
+      audio.play();
+
+      break;
+
+    default:
+      console.log(key);
+      break;
+  }
+}
+function buttonAnimation(currentKey) {
+  var activeButton = document.querySelector("." + currentKey);
+  activeButton.classList.add("pressed");
+  setTimeout(function () {
+    activeButton.classList.remove("pressed");
+  }, 100);
+}
